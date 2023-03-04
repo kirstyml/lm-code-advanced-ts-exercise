013689cc53751190d7cf2ec789694fd2ab838886
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-03-04 10:37:07 +0000

    fix: made baselurl use server config

--- client/src/api/base_url.ts
+++ client/src/api/base_url.ts
@@ -1,4 +1,8 @@
 // ❗ this won't always be true!
 //    we should add some ability to configure the client with the
 //    correct baseUrl...
-export const baseUrl = "http://localhost:8080";
+import { getConfig } from "../../../server/src/config/config";
+const config = getConfig();
+const devServer = `http://localhost:${config.port}`;
+const prodServer = "someProductionServer";
+export const baseUrl = config.isDev ? devServer: prodServer;
