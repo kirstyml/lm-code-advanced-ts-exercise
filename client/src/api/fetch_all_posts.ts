0b9a356062b0634fadf1a64456fcffd437d20d8b
Kirsty Leishman <leishman.kirsty@gmail.com>
2023-03-04 10:37:39 +0000

    fix: added types to fetch all users and posts

--- client/src/api/fetch_all_posts.ts
+++ client/src/api/fetch_all_posts.ts
@@ -1,10 +1,11 @@
 import { baseUrl } from "./base_url";
+import { Post } from "../../../server/src/types/posts.types";
 
 export async function fetchAllPosts() {
 	try {
 		const result = await fetch(baseUrl + "/api/posts/all");
 		const posts = await result.json();
-		return posts;
+		return posts as Post[];
 	} catch {
 		return [];
 	}
--- client/src/api/fetch_all_users.ts
+++ client/src/api/fetch_all_users.ts
@@ -1,10 +1,11 @@
 import { baseUrl } from "./base_url";
+import { User } from "../../../server/src/types/posts.types";
 
 export async function fetchAllUsers() {
 	try {
 		const result = await fetch(baseUrl + "/api/users/all");
 		const users = await result.json();
-		return users;
+		return users as User[];
 	} catch {
 		return [];
 	}
